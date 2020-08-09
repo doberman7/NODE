b43625350a7f18dc42e3e4a0f906b26221e6fa2d
Ivan Rubio <ivanrubioangeles@gmail.comn>
2020-08-09 13:16:14 -0500

    analizing data and doing POST requests test

--- second_server/main.js
+++ second_server/main.js
@@ -1,22 +1,40 @@
 const port = 3000,
+  http = require("http"),
+  httpStatus = require("http-status-codes"),
+  app = http.createServer();
+//listen for Request
+app.on("request", (req, res) => {
+  //create an array to hold chunk contents
+  var body = [];
+  //process it in another callback function
+  req.on("data", (bodyData) => {
+    //add recibed data to the body array
+    body.push(bodyData);
+  });
+  //run code when data transmision ends
+  req.on("end", ()=> {
+    //convert the body array to a string of text
+    body = Buffer.concat(body).toString();
+    //log the request's content to your console
+    console.log(`Request Body countents: ${body}`);
+  });
 
-	http = require("http"),
-	httpStatus = require("http-status-codes"),
-	app = http.createServer((request, response) => {
-		console.log("Received an incoming request!");
-		response.writeHead(httpStatus.OK, {
-			"Content-Type": "text/html"
-		});
-		
-		let responseMessage = "<h1>Hello, Universe!</h1>";
-		response.write(responseMessage);
-		response.end();
-		console.log(`Sent a response : ${responseMessage}`);
-	});
+  const getJSONString = obj => {
+    return JSON.stringify(obj,null, 2);
+  }
 
-	
-	
-	app.listen(port);
-	console.log(`The server has started and is listening on port number:
-	${port}`);
+  console.log(`Method: ${getJSONString(req.method)}`);
+  console.log(`URL: ${getJSONString(req.url)}`);
+  console.log(`Headers: ${getJSONString(req.headers)}`);
 
+  res.writeHead(httpStatus.OK, {
+    "Content-Type": "text/html"
+  });
+//prepare response
+  let responseMessage = "<h1>This will show on the screen.</h1>";
+  res.end(responseMessage);
+});
+//response with html
+  app.listen(port);
+  console.log(`The server has started and is listening on port number:
+   ${port}`);
--- second_server/main2.js
+++ second_server/main2.js
@@ -3,13 +3,23 @@ const port = 3000,
   httpStatus = require("http-status-codes"),
   app = http.createServer();
 //server fires the code in callback function when a request event is trigered
-app.on("request", (req, res) => {
+app.on("request", (req, res) => {  
   res.writeHead(httpStatus.OK, {
     "Content-Type": "text/html"
   });
 //prepare response
   let responseMessage = "<h1>This will show on the screen.</h1>";
   res.end(responseMessage);
+  //log the HTTP method
+  console.log(req.method);
+  //the request url
+  console.log(req.url);
+  //log the request headers
+  console.log(req.headers);
+  //convert js into to string
+  const getJSONString = obj => {
+    return JSON.stringify(obj,null, 2);
+  }
 });
 //response with html
   app.listen(port);
