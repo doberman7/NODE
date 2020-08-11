f359c331460c41ecb0de60a83fe9247548a12c8d
Ivan Rubio <ivanrubioangeles@gmail.comn>
2020-08-11 17:57:49 -0500

    end to lesson8

--- first_express_project/main.js
+++ first_express_project/main.js
@@ -4,10 +4,14 @@ const port = 3000,
   chalkAnimation = require('chalk-Animation')
 
 app.get("/", (req, res) => {//set a get route to for the home page
+  console.log(req.params);
+  console.log(req.body);
+  console.log(req.url);
+  console.log(req.query);
   res.send("Hello, Universe!");//issue a response from the server to the client with res.send
+
 })
 .listen(port, () => {//seth the app to listen port 3000
   //console.log(`The Express.js server has started and is listening  on port number: ${port}`);
-  chalkAnimation.rainbow(`The Express.js server has
-    started and is listening  on port number: ${port}`)
+  chalkAnimation.radar(`The Express.js server has started and is listening  on port number: ${port}`)
 });
