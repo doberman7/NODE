91175e3a99c22663e6d2f24bd83ed24a6dc20717
Ivan Rubio <ivanrubioangeles@gmail.comn>
2020-08-11 15:50:09 -0500

    runing local host without querys, bit colored string added on  terminal at running server whit chalk

---
+++ confetti_cuisine/router.js
@@ -0,0 +1,25 @@
+const httpStatus = require("http-status-codes"),
+  contentTypes = require("./contentTypes"),
+  utils = require("./utils");
+//create routes objects to hold routes functions
+const routes = {
+  "GET": {},
+  "POST": {}
+};
+//create the handle function to handle request
+exports.handle = (req, res) => {
+  try {
+    routes[req.method][req.url](req, res);
+  } catch (e) {
+    res.writeHead(httpStatus.OK, contentTypes.html);
+    utils.getFile("views/error.html", res);
+  }
+};
+//create the get and post functions to map route functions
+exports.get = (url, action) => {
+  routes["GET"][url] = action;
+};
+
+exports.post = (url, action) => {
+  routes["POST"][url] = action;
+};
